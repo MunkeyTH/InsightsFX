49e17810eca3ac4538842fc9ba9bcc86c4c3ab78
Munkey <tim.harrison3@googlemail.com>
2018-08-02 20:23:42 +0100

    First Commit

---
+++ InsightsFX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsightsFX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
